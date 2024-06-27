1ebd426a61cc2fd383bc6d72f1fd1d31f85cc7bb
hefusen <3580476060@qq.com>
2024-06-27 21:33:02 +0800

    add testing examples for GNNExplainer.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # replicate_gnn_explainer
+
+## Setup
+
+```commandline
+conda install pytorch torchvision torchaudio cpuonly -c pytorch
+conda install pyg -c pyg
+```
+
+## Examples
+
+main.py -- GNNExplainer for node classification task
+
+Results are saved in feature_importance.png and subgraph.
--- main.py
+++ main.py
@@ -1,16 +1,64 @@
-# This is a sample Python script.
+import os.path as osp
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import torch
+import torch.nn.functional as F
+
+from torch_geometric.datasets import Planetoid
+from explain import Explainer, GNNExplainer
+from torch_geometric.nn import GCNConv
+
+dataset = 'Cora'
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Planetoid')
+dataset = Planetoid(path, dataset)
+data = dataset[0]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class GCN(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = GCNConv(dataset.num_features, 16)
+        self.conv2 = GCNConv(16, dataset.num_classes)
+
+    def forward(self, x, edge_index):
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = GCN().to(device)
+data = data.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for epoch in range(1, 201):
+    model.train()
+    optimizer.zero_grad()
+    out = model(data.x, data.edge_index)
+    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+    loss.backward()
+    optimizer.step()
+
+explainer = Explainer(
+    model=model,
+    algorithm=GNNExplainer(epochs=200),
+    explanation_type='model',
+    node_mask_type='attributes',
+    edge_mask_type='object',
+    model_config=dict(
+        mode='multiclass_classification',
+        task_level='node',
+        return_type='log_probs',
+    ),
+)
+node_index = 10
+explanation = explainer(data.x, data.edge_index, index=node_index)
+print(f'Generated explanations in {explanation.available_explanations}')
+
+path = 'feature_importance.png'
+explanation.visualize_feature_importance(path, top_k=10)
+print(f"Feature importance plot has been saved to '{path}'")
+
+path = 'subgraph.pdf'
+explanation.visualize_graph(path)
+print(f"Subgraph visualization plot has been saved to '{path}'")
